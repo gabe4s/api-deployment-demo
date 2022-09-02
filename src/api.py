78102c214b9f230c4748b49e281dedce3ab0b1fa
Gabe Douda <gabedouda@gmail.com>
2022-09-02 11:07:38 -0600

    update test

--- src/api.py
+++ src/api.py
@@ -2,13 +2,13 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
-def index():
-    return jsonify({'input': request.args.get('input')})
-
 @app.route('/test', methods = ['GET'])
 def index():
     return "updated 4"
+
+@app.route('/', methods = ['GET'])
+def index():
+    return jsonify({'input': request.args.get('input')})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
