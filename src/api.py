ecfde2c54e182171c229b611af3daea5054ed2a8
Gabe Douda <gabedouda@gmail.com>
2022-09-02 11:12:26 -0600

    pre-deployment-test

--- src/api.py
+++ src/api.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def index():
     return jsonify({'input': request.args.get('input')})
 
-@app.route('/test', methods = ['GET'])
-def test():
-    return "Hello DevOps Team"
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
