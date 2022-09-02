4b76e2c1167cc6cee41c988d6aa289571bb81e50
Gabe Douda <gabedouda@gmail.com>
2022-09-02 11:10:36 -0600

    update test

--- src/api.py
+++ src/api.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/test', methods = ['GET'])
 def test():
-    return "updated 4"
+    return "Hello DevOps Team"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
